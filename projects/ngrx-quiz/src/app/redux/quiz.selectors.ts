604f64a2b45f5430a23299d4c8c94fa3121f2fc5
Kobi Hari <kobi2294@yahoo.com>
2020-07-07 18:15:56 +0300

    Added first selectors

---
+++ projects/ngrx-quiz/src/app/redux/quiz.selectors.ts
@@ -0,0 +1,16 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { QuizState, QuizFeatureKey } from './quiz.model';
+
+export const selectState = 
+    createFeatureSelector<QuizState>(QuizFeatureKey);
+
+export const selectCurrentQuestionIndex = createSelector(
+    selectState, 
+    state => state.answers.findIndex(answer => answer.userAnswer === null)
+);
+
+export const selectCurrentQuestion = createSelector(
+    selectState, 
+    selectCurrentQuestionIndex, 
+    (state, index) => state.questions[index]
+);
