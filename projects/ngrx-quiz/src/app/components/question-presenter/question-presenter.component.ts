cbc00c2bb5c6192afde42dcfac471e8983ad80e9
Kobi Hari <kobi2294@yahoo.com>
2020-07-07 17:46:54 +0300

    Question Presenter implementation

--- projects/ngrx-quiz/src/app/components/question-presenter/question-presenter.component.ts
+++ projects/ngrx-quiz/src/app/components/question-presenter/question-presenter.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question-presenter.component.scss']
 })
 export class QuestionPresenterComponent implements OnInit {
+  caption: string = 'What do you get when you mix red and yellow';
+  answers: string[] = [
+    'White', 
+    'Orange', 
+    'Pink', 
+    'Green'
+  ];
+
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
